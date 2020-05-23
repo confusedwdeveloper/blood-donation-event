e26e4935dc4ff6ff1c64321e94365c34b108ebbf
Darshan Datta <darshandatta.10@gmail.com>
2020-05-23 21:54:04 +0530

    created a middleware to verify token

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,21 @@
+const jwt = require("jwt");
+const config = require("config");
+
+module.exports = function verifyToken(req, res, next) {
+  // get token
+  const token = req.token("x-auth-token");
+  if (!token) {
+    return res.status(400).json({ msg: "No token, access denied" });
+  }
+
+  try {
+    // verify token
+    const secret = config.get("jwtSecret");
+    const payload = jwt.verify(token, secret);
+    req.user = payload.user;
+    next();
+  } catch (err) {
+    console.error(err.message);
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
